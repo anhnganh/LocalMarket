ce0445c49b39117f6a7488d0033ec3b37beb74c1
Nguyen Thanh Huy <huynt091100@gmail.com>
2021-07-06 09:43:38 +0700

    finish shopping cart

--- src/router/index.js
+++ src/router/index.js
@@ -1,81 +1,85 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Home from "../views/Home.vue";
 // import About from '../views/NguoiBan.vue'
-import ManagerShop from '../views/admin/ManagerShop.vue'
-import Contact from '../views/user/Contact.vue'
-import productList from '../views/user/ProductList'
-import NewList from '../views/user/NewList.vue'
-import RevenueManager from '../views/admin/revenueStatistics.vue'
-import ProductManager from '../views/admin/ManageProduct.vue'
-import ShopDetail from '../views/user/Shop_Detail.vue'
+import ManagerShop from "../views/admin/ManagerShop.vue";
+import Contact from "../views/user/Contact.vue";
+import productList from "../views/user/ProductList";
+import NewList from "../views/user/NewList.vue";
+import RevenueManager from "../views/admin/revenueStatistics.vue";
+import ProductManager from "../views/admin/ManageProduct.vue";
+import ShopDetail from "../views/user/Shop_Detail.vue";
+import ShoppingCart from "../views/user/ShoppingCart.vue";
 
-import LoginUser from '../views/user/abc.vue'
+import LoginUser from "../views/user/abc.vue";
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
+    path: "/",
+    name: "Home",
+    component: Home,
   },
   {
-    path: '/nguoiban',
-    name: 'About',
+    path: "/nguoiban",
+    name: "About",
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import("@/views/NguoiBan.vue")
+    component: () => import("@/views/NguoiBan.vue"),
   },
   {
-    path:'/managershop',
-    name: 'ManagerShop',
-    component: ManagerShop
+    path: "/managershop",
+    name: "ManagerShop",
+    component: ManagerShop,
   },
   {
-    path:'/contact',
-    name:'Contact',
-    component:Contact
+    path: "/contact",
+    name: "Contact",
+    component: Contact,
   },
   {
-    path:'/productList',
-    name: 'productList',
-    component: productList
+    path: "/productList",
+    name: "productList",
+    component: productList,
   },
   {
-    path:'/newlist',
-    name:NewList,     
-    component: NewList
+    path: "/newlist",
+    name: NewList,
+    component: NewList,
   },
   {
-    path:'/revenueManager',
+    path: "/revenueManager",
     name: RevenueManager,
-    component: RevenueManager
+    component: RevenueManager,
   },
   {
-    path:'/ProductManager',
+    path: "/ProductManager",
     name: ProductManager,
-    component: ProductManager
+    component: ProductManager,
   },
   {
-    path:'/shopDetail',
+    path: "/shopDetail",
     name: ShopDetail,
-    component:ShopDetail
+    component: ShopDetail,
   },
   {
-    path:'/login',
-    name:LoginUser,
-    component: LoginUser
-  }  
-
-
-]
+    path: "/login",
+    name: LoginUser,
+    component: LoginUser,
+  },
+  {
+    path: "/shoppingCart",
+    name: "ShoppingCart",
+    component: ShoppingCart,
+  },
+];
 
 const router = new VueRouter({
-  mode: 'history',
+  mode: "history",
   base: process.env.BASE_URL,
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
